a08fcc8f65e2a8488c143d7c42e053e358f08cd5
KenyonBL <157287613+KenyonBL@users.noreply.github.com>
2024-02-05 21:06:00 -0500

    complete code

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,10 @@
 def searchPrefix(arr,s)
-
+    items = arr.select {|item| item.start_with? (s)}
+    return items
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
-
+print ("\n")
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+print ("\n")
+print(searchPrefix(["ccdfg", "ccrtyeu", "adeab", "jdkfhdsgj", "ccmkliu", "gsjgddeu"],"cc"))
